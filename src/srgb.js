f087877e51aeb7e3ffce62bec2bff75ebf64f3b5
Tim Janik <timj@gnu.org>
2022-01-27 21:50:38 +0100

    SRC: srgb.js: srgb_hex(): handle RGBA strings
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/srgb.js
+++ src/srgb.js
@@ -2,14 +2,20 @@
 'use strict';
 
 /// Create hex string from sRGB array.
-export function srgb_hex (a) {
-  const r = (255 * a[0] + 0.5 |0).toString (16);
-  const g = (255 * a[1] + 0.5 |0).toString (16);
-  const b = (255 * a[2] + 0.5 |0).toString (16);
+export function srgb_hex (rgb) {
+  const r = (255 * rgb[0] + 0.5 |0).toString (16);
+  const g = (255 * rgb[1] + 0.5 |0).toString (16);
+  const b = (255 * rgb[2] + 0.5 |0).toString (16);
   const r0 = r.length > 1 ? r : '0' + r;
   const g0 = g.length > 1 ? g : '0' + g;
   const b0 = b.length > 1 ? b : '0' + b;
-  return '#' + r0 + g0 + b0;
+  let hex = '#' + r0 + g0 + b0;
+  if (rgb.length > 3) {
+    const a = (255 * rgb[3] + 0.5 |0).toString (16);
+    const a0 = a.length > 1 ? a : '0' + a;
+    hex += a;
+  }
+  return hex;
 }
 
 /// Create sRGB from color string, integer or vector `[ 0…1, 0…1, 0…1 ]`
