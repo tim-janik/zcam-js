5f150d291aa769ce7339630d915de46e8471af6a
Tim Janik <timj@gnu.org>
2022-01-27 18:31:54 +0100

    SRC: check.js: create script for unit tests
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ src/check.js
@@ -0,0 +1,12 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+'use strict';
+
+import * as assert from 'assert';
+import * as M from './math.js';
+
+// === math.js tests ==
+function test_math () {
+}
+
+// Run unit tests
+test_math();
