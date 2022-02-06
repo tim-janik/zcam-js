3af41103f063df41efaf35a2252e9f10ad7977ab
Tim Janik <timj@gnu.org>
2022-02-06 18:19:30 +0100

    SRC: math.js: add matrix33_mul3()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/math.js
+++ src/math.js
@@ -1,2 +1,13 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 'use strict';
+
+
+/// Multiply 3x3 matrix by 3 element vector.
+export function matrix33_mul3 (A, V) {
+  const [[a,b,c], [d,e,f], [g,h,i]] = A;
+  const [x,y,z] = V;
+  const u = a*x + b*y + c*z;
+  const v = d*x + e*y + f*z;
+  const w = g*x + h*y + i*z;
+  return [u,v,w];
+}
