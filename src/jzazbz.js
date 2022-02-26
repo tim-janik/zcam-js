a0eb06515059c801c238567fcc4a9446abcdbcc1
Tim Janik <timj@gnu.org>
2022-02-26 14:36:50 +0100

    SRC: jzazbz.js: turn Jzazbz_M1, Jzazbz_R1, Jzazbz_R2 into module constants
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/jzazbz.js
+++ src/jzazbz.js
@@ -7,6 +7,10 @@ import * as M from './math.js';
 const Jzazbz_b = 1.15, Jzazbz_g = 0.66, Jzazbz_d0 = 1.6295499532821566e-11;
 const Jzazbz_η = 2610 / 2**14, Jzazbz_ρ = 1.7 * 2523 / 2**5;
 const Jzazbz_c1 = 3424 / 2**12, Jzazbz_c2 = 2413 / 2**7, Jzazbz_c3 = 2392 / 2**7;
+const Jzazbz_M1 = [ [0.41478972, 0.579999, 0.0146480], [-0.2015100, 1.120649, 0.0531008], [-0.0166008, 0.264800, 0.6684799] ];
+const Jzazbz_R1 = [ [ 1.9242264357876069,   -1.0047923125953657,   0.037651404030618014],
+		    [ 0.35031676209499912,   0.72648119393165533, -0.065384422948085025 ],
+		    [-0.090982810982847592, -0.31272829052307399,  1.5227665613052606 ] ]; // == Jzazbz_M1^-1
 
 /// Convert from absolute XYZ (D65/2°) to Jzazbz color space.
 export function Jzazbz_from_xyz ({ x: x65, y: y65, z: z65 }) {
@@ -14,8 +18,7 @@ export function Jzazbz_from_xyz ({ x: x65, y: y65, z: z65 }) {
   // https://doi.org/10.1364/OE.25.015131
   const X_D65 = Jzazbz_b*x65 - (Jzazbz_b-1)*z65;
   const Y_D65 = Jzazbz_g*y65 - (Jzazbz_g-1)*x65;
-  const Mlms = [ [ 0.41478972, 0.579999, 0.0146480 ], [ -0.2015100, 1.120649, 0.0531008 ], [ -0.0166008, 0.264800, 0.6684799 ] ];
-  const LMS = M.matrix33_mul3 (Mlms, [X_D65, Y_D65, z65]);
+  const LMS = M.matrix33_mul3 (Jzazbz_M1, [X_D65, Y_D65, z65]);
   const PQ = v => {
     const vη = (v / 10000)**Jzazbz_η;
     return ((Jzazbz_c1 + Jzazbz_c2 * vη) / (1 + Jzazbz_c3 * vη))**Jzazbz_ρ;
@@ -32,19 +35,16 @@ export function Jzazbz_from_xyz ({ x: x65, y: y65, z: z65 }) {
 export function xyz_from_Jzazbz ({ j: Jz, a: az, b: bz }) {
   const Jzd0 = Jz + Jzazbz_d0;
   const Iz = Jzd0 / (0.44 + 0.56 * Jzd0);
-  const L_ = Iz +1.386050432715393e-1 * az +5.804731615611886e-2 * bz;
-  const M_ = Iz -1.386050432715393e-1 * az -5.804731615611886e-2 * bz;
-  const S_ = Iz -9.601924202631894e-2 * az -8.118918960560388e-1 * bz;
+  const L_ = Iz +0.13860504327153927  * az +0.058047316156118862 * bz;
+  const M_ = Iz -0.13860504327153927  * az -0.058047316156118862 * bz;
+  const S_ = Iz -0.096019242026318938 * az -0.81189189605603884  * bz;
   const iη = 1 / Jzazbz_η, iρ = 1 / Jzazbz_ρ;
   const PQinv = v => {
     const vq = v**iρ;
     return 10000 * ((Jzazbz_c1 - vq) / (Jzazbz_c3 * vq - Jzazbz_c2))**iη;
   };
   const RGB = [L_, M_, S_].map (PQinv);
-  const Mxyz = [ [ 1.924226435787607e0,  -1.004792312595366e0,   3.765140403061801e-2 ],
-                 [ 3.503167620949991e-1,  7.264811939316553e-1, -6.538442294808503e-2 ],
-                 [-9.098281098284759e-2, -3.12728290523074e-1,   1.522766561305261e0  ] ];
-  const [X_, Y_, Z] = M.matrix33_mul3 (Mxyz, RGB);
+  const [X_, Y_, Z] = M.matrix33_mul3 (Jzazbz_R1, RGB);
   const X = 1/Jzazbz_b * (X_ + (Jzazbz_b-1) * Z);
   const Y = 1/Jzazbz_g * (Y_ + (Jzazbz_g-1) * X);
   return { x: X, y: Y, z: Z };
