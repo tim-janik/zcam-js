5e6ed04b9f707603886ba513b3e8bbd0c8611bbe
Tim Janik <timj@gnu.org>
2022-03-02 01:57:44 +0100

    SRC: check.js: add bsearch_max()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/check.js
+++ src/check.js
@@ -4,8 +4,14 @@
 import * as assert from 'assert';
 import * as M from './math.js';
 
+/// Round to a given number of digits.
+const rnd = (v, digits = 0) => Math.round (v * 10**digits) / 10**digits;
+const rnd2 = v => rnd (v, 2), rnd3 = v => rnd (v, 3);
+const rnd5 = v => rnd (v, 5), rnd7 = v => rnd (v, 7);
+
 // === math.js tests ==
 function test_math () {
+  assert.deepEqual (rnd5 (M.bsearch_max (x => x <= 1, -5, +5)), 1.0);
 }
 
 // Run unit tests
