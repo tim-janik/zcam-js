81fe4db9734692bbd11c35c74275de3183e98d65
Tim Janik <timj@gnu.org>
2022-03-12 03:50:23 +0100

    SRC: math.js: move inner function definition
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/math.js
+++ src/math.js
@@ -132,22 +132,22 @@ export class CubicSpline {
     return this.a[0];
   }
   splint_newint (t) {
+    function newint (x, x0, x1, y0, y1, sg0, sg1) { // 2020, Fast Cubic Spline Interpolation, Haysn Hornbeck
+      const h = (x1 - x0);
+      const wh = (x - x0);
+      const inv_h = 1. / h;
+      const bx = (x1 - x);
+      const h2 = h * h;			// 3 adds , 1 mult , 1 div
+      const lower = wh * y1 + bx * y0;
+      const C = (wh * wh - h2) * wh * sg1;
+      const D = (bx * bx - h2) * bx * sg0;	// 1 add , 2 subs , 8 mults
+      // 2 adds , 2 mult = 19 ops + 1 div
+      return (lower + /* (1/6)* */ (C + D)) * inv_h;
+    }
     const x = this.x;
     for (let i = x.length - 2; i >= 0; i--)
       if (x[i] <= t) {
 	return newint (t, x[i], x[i+1], this.a[i], this.a[i+1], this.sg[i], this.sg[i+1]);
-	function newint (x, x0, x1, y0, y1, sg0, sg1) { // 2020, Fast Cubic Spline Interpolation, Haysn Hornbeck
-	  const h = (x1 - x0);
-	  const wh = (x - x0);
-	  const inv_h = 1. / h;
-	  const bx = (x1 - x);
-	  const h2 = h * h;			// 3 adds , 1 mult , 1 div
-	  const lower = wh * y1 + bx * y0;
-	  const C = (wh * wh - h2) * wh * sg1;
-	  const D = (bx * bx - h2) * bx * sg0;	// 1 add , 2 subs , 8 mults
-	  // 2 adds , 2 mult = 19 ops + 1 div
-	  return (lower + /* (1/6)* */ (C + D)) * inv_h;
-	}
       }
     return this.a[0];
   }
