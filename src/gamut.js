2be3955978e98c933bb36708cc8e436617a3aedb
Tim Janik <timj@gnu.org>
2022-03-12 20:17:04 +0100

    SRC: gamut.js: add cache_cusps()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- src/gamut.js
+++ src/gamut.js
@@ -8,6 +8,11 @@ import * as Z from './zcam.js';
 export class Gamut {
   constructor (zcamviewingconditions = {}) {
     this.viewing = Z.zcam_setup (zcamviewingconditions);
+    this.Jz_spline = null;
+    this.Cz_spline = null;
+    this.extrema = [];
+    this.minCz = NaN;
+    this.maxCz = NaN;
   }
   /// Retrieve sRGB coordinates and assign `inside` to true if within 8bit sRGB gamut.
   contains (zcam) {
@@ -18,9 +23,101 @@ export class Gamut {
   zcam (srgb) {
     return Z.zcam_from_srgb (srgb);
   }
+  async cache_cusps (cfg = {}) {
+    const fdump = cfg.fdump;
+    if (this.Cz_spline) return;
+    await 0; // distribute slow work
+    const viewing = this.viewing;
+    // calculation accuracy of reference values
+    const cusp_epsilon = 1e-5;
+    // accuracy for spline control point selection
+    const spline_epsilon = 1e-3;
+
+    // find hues with exact min/max extrema of Jz
+    const hue_find_cusp = h => Z.zcam_hue_find_cusp (h, cusp_epsilon, 101, viewing);
+    const spots = [ 42, 102, 133, 204, 258, 321, 402 ]; // rough guesses
+    for (const [i, spot] of spots.entries()) {
+      const gss_minmax = i % 2 ? M.gss_max : M.gss_min;
+      // accuracy is important for spline segmentation
+      const {x, y} = gss_minmax (h => hue_find_cusp (h).Jz, spot - 22, spot + 22, 1e-7);
+      this.extrema.push (x);
+      await 0; // distribute slow work
+    }
+
+    // calculate hue samples close to extrema and at equidistant positions
+    let xs = [...this.extrema], ys, zs = [], dstep = 0.5;
+    for (const x of this.extrema) {
+      // offset epsilon by more than *10 to avoid perturbations due to interval nesting inaccuracies
+      for (let d = cusp_epsilon * 100; d < dstep; d = d * 2) {
+	xs.push (x - d);
+	xs.push (x + d);
+      }
+    }
+    for (let i = 0; i < this.extrema[this.extrema.length -1]; i += dstep)
+      xs.push (i);
+    xs = xs.sort ((a, b) => a - b).filter ((x,i,a) => !i || x != a[i-1]); // sort | uniq
+    // Calculate cusp samples
+    fdump && console.log ("Calculate cusp samples:", xs.length + '…');
+    for (let i = 0; i < xs.length; i++) {
+      zs.push (hue_find_cusp (xs[i]));
+      if (0 == (i & 0x1f))
+	await 0; // distribute slow work
+    }
+    await 0; // distribute slow work
+
+    // Jz Spline
+    ys = zs.map (z => z.Jz);
+    this.Jz_spline = fit_spline_segments (xs, ys, this.extrema, spline_epsilon, 256, this.extrema,
+					  !fdump ? null : (n, d) => fdump ('xg-jz' + n, d));
+    fdump && fdump ('xg-jzf', xs.map ((x, i) => x + ' ' + ys[i]).join ('\n')); // Jz function
+    await 0; // distribute slow work
+
+    // Cz Spline
+    ys = zs.map (z => z.Cz);
+    this.Cz_spline = fit_spline_segments (xs, ys, this.extrema, spline_epsilon, 256, this.extrema,
+					  !fdump ? null : (n, d) => fdump ('xg-cz' + n, d));
+    fdump && fdump ('xg-czf', xs.map ((x, i) => x + ' ' + ys[i]).join ('\n')); // Cz function
+    await 0; // distribute slow work
+
+    // Cz maximum
+    this.minCz = Array.from (this.Cz_spline.a).reduce ((p, n) => Math.min (p, n));
+    this.maxCz = Array.from (this.Cz_spline.a).reduce ((p, n) => Math.max (p, n));
+  }
 }
 
+// == Spline fitting ==
+function fit_spline_segments (xs, ys, segpointes, eps, pmax, fixed = [], fdump) {
+  const spline = new M.CubicSpline();
+  for (let j = 1; j < segpointes.length; j++) {
+    const spline_xs = [], spline_ys = [];
+    for (let i = 0; i < xs.length; i++)
+      if (xs[i] >= segpointes[j-1] && xs[i] <= segpointes[j]) {
+	spline_xs.push (xs[i]);
+	spline_ys.push (ys[i]);
+      }
+    let { spline: seg, diff } = M.spline_fit (spline_xs, spline_ys, eps, 37, fixed);
+    if (fdump) console.log ("segment:", "points=" + seg.x.length, "diff=" + diff);
+    spline.add_segment (seg.x, seg.a);
+  }
+  if (fdump) {
+    console.log ("Spline:", "points=" + spline.x.length);
+    const vs = [], K = 57;
+    for (let i = 1; i < spline.x.length; i++)
+      for (let j = 0; j <= K; j++) {
+	const x = spline.x[i-1] + (spline.x[i] - spline.x[i-1]) * j / K;
+	const y = spline.splint (x);
+	vs.push (x + ' ' + y);
+      }
+    fdump ('s', vs.join ('\n')); // points
+    fdump ('p', Array.from (spline.x).map ((v, i) => v + ' ' + spline.a[i]).join ('\n') + '\n'); // spline eval
+  }
+  return spline;
+}
+
+// == test ==
 async function test () {
+  let FS = null;        // use module 'fs' to create gnuplot files
+  //FS = await import ('fs');
   const rnd = (v, digits = 0) => Math.round (v * 10**digits) / 10**digits;
   const rnd2 = v => rnd (v, 2), rnd3 = v => rnd (v, 3);
   const assert = await import ('assert');
@@ -32,7 +129,18 @@ async function test () {
   assert.deepEqual (c.inside, true); // is inside
   assert.deepEqual (c.b > c.g && c.b > c.r, true); // is blue
   assert.deepEqual (rnd2 (g.zcam ('#ff0000').hz), 42.48); // is red
+  function fdump (filename, contents) {
+    FS.writeFileSync (filename, contents);
+    console.log (filename, contents.length, "bytes");
+  }
+  await g.cache_cusps ({
+    fdump: FS ? fdump : null,
+  });
+
+  if (FS)
+    console.log (`plot "xg-jzf" with lines, "xg-jzs" with lines, "xg-jzp", "xg-czf" with lines, "xg-czs" with lines, "xg-czp", ${g.minCz}, ${g.maxCz}`);
 }
 
+// nodejs __main__ check
 if (process.argv[1] == import.meta.url.replace (/^file:\/\//, ''))
   await test();
